17a2c56147ea225f1370c2948c449e13b3a48d29
Collin Wu <collinwu@users.noreply.github.com>
2022-12-10 21:06:16 -0800

    chore(tests): update tests formatting

--- tests/cleaner.test.ts
+++ tests/cleaner.test.ts
@@ -1,149 +1,149 @@
-import cheerio from 'cheerio';
-import fs from 'fs';
-import cleaner from '../src/cleaner';
+import cheerio from "cheerio";
+import fs from "fs";
+import cleaner from "../src/cleaner";
 
-describe('Cleaner', () => {
-  it('exists', () => {
+describe("Cleaner", () => {
+  test("exists", () => {
     expect(cleaner).toBeDefined();
   });
 
-  it('removes body classes', () => {
+  test("removes body classes", () => {
     const html = fs
-      .readFileSync('tests/fixtures/test_businessWeek1.html')
+      .readFileSync("tests/fixtures/test_businessWeek1.html")
       .toString();
     const originalDoc = cheerio.load(html);
 
-    expect(originalDoc('body')?.attr('class')?.trim()).toEqual('magazine');
+    expect(originalDoc("body")?.attr("class")?.trim()).toEqual("magazine");
 
     const newDoc = cleaner(originalDoc);
-    expect(newDoc('body')?.attr('class')).toEqual('');
+    expect(newDoc("body")?.attr("class")).toEqual("");
   });
 
-  it('removes article attributes', () => {
+  test("removes article attributes", () => {
     const html = fs
-      .readFileSync('tests/fixtures/test_gizmodo1.html')
+      .readFileSync("tests/fixtures/test_gizmodo1.html")
       .toString();
     const originalDoc = cheerio.load(html);
 
-    expect(originalDoc('article')?.attr('class')?.trim()).toEqual(
-      'row post js_post_item status-published commented js_amazon_module'
+    expect(originalDoc("article")?.attr("class")?.trim()).toEqual(
+      "row post js_post_item status-published commented js_amazon_module"
     );
 
     const newDoc = cleaner(originalDoc);
-    expect(newDoc('article')?.attr('class')).toBeUndefined();
+    expect(newDoc("article")?.attr("class")).toBeUndefined();
   });
 
-  it('removes em tag from image-less ems', () => {
-    const html = fs.readFileSync('tests/fixtures/test_gizmodo1.html');
+  test("removes em tag from image-less ems", () => {
+    const html = fs.readFileSync("tests/fixtures/test_gizmodo1.html");
     const originalDoc = cheerio.load(html);
 
-    expect(originalDoc('em').length).toEqual(6);
+    expect(originalDoc("em").length).toEqual(6);
 
     const newDoc = cleaner(originalDoc);
-    expect(newDoc('em').length).toEqual(0);
+    expect(newDoc("em").length).toEqual(0);
   });
 
-  it('removes scripts', () => {
+  test("removes scripts", () => {
     const html = fs
-      .readFileSync('tests/fixtures/test_businessWeek1.html')
+      .readFileSync("tests/fixtures/test_businessWeek1.html")
       .toString();
     const originalDoc = cheerio.load(html);
 
-    expect(originalDoc('script').length).toEqual(40);
+    expect(originalDoc("script").length).toEqual(40);
 
     const newDoc = cleaner(originalDoc);
-    expect(newDoc('script').length).toEqual(0);
+    expect(newDoc("script").length).toEqual(0);
   });
 
-  it('removes comments', () => {
+  test("removes comments", () => {
     const html = fs
-      .readFileSync('tests/fixtures/test_gizmodo1.html')
+      .readFileSync("tests/fixtures/test_gizmodo1.html")
       .toString();
     const originalDoc = cheerio.load(html);
 
-    const comments = originalDoc('*')
+    const comments = originalDoc("*")
       .contents()
       .filter((_index, element) => {
-        return element.type === 'comment';
+        return element.type === "comment";
       });
 
     expect(comments.length).toEqual(15);
 
     const newDoc = cleaner(originalDoc);
-    const newComments = newDoc('*')
+    const newComments = newDoc("*")
       .contents()
       .filter((_index, element) => {
-        return element.type === 'comment';
+        return element.type === "comment";
       });
 
     expect(newComments.length).toEqual(0);
   });
 
-  it('replaces childless divs with p tags', () => {
+  test("replaces childless divs with p tags", () => {
     const originalDoc = cheerio.load(
-      '<html><body><div>text1</div></body></html>'
+      "<html><body><div>text1</div></body></html>"
     );
     const newDoc = cleaner(originalDoc);
 
-    expect(newDoc('div').length).toEqual(0);
-    expect(newDoc('p').length).toEqual(1);
-    expect(newDoc('p').text()).toEqual('text1');
+    expect(newDoc("div").length).toEqual(0);
+    expect(newDoc("p").length).toEqual(1);
+    expect(newDoc("p").text()).toEqual("text1");
   });
 
-  it('replaces u tags with plain text', () => {
-    const originalDoc = cheerio.load('<html><body><u>text1</u></body></html>');
+  test("replaces u tags with plain text", () => {
+    const originalDoc = cheerio.load("<html><body><u>text1</u></body></html>");
     const newDoc = cleaner(originalDoc);
 
-    expect(newDoc('u').length).toEqual(0);
-    expect(newDoc('body').html()).toEqual('text1');
+    expect(newDoc("u").length).toEqual(0);
+    expect(newDoc("body").html()).toEqual("text1");
   });
 
-  it('removes divs by regex (ex: /caption/)', () => {
-    const html = fs.readFileSync('tests/fixtures/test_aolNews.html').toString();
+  test("removes divs by regex (ex: /caption/)", () => {
+    const html = fs.readFileSync("tests/fixtures/test_aolNews.html").toString();
     const originalDoc = cheerio.load(html);
-    const captions = originalDoc('div.caption');
+    const captions = originalDoc("div.caption");
     expect(captions.length).toEqual(1);
 
     const newDoc = cleaner(originalDoc);
-    const newCaptions = newDoc('div.caption');
+    const newCaptions = newDoc("div.caption");
     expect(newCaptions.length).toEqual(0);
   });
 
-  it('removes naughty elements by regex (ex: /caption/)', () => {
-    const html = fs.readFileSync('tests/fixtures/test_issue28.html').toString();
+  test("removes naughty elements by regex (ex: /caption/)", () => {
+    const html = fs.readFileSync("tests/fixtures/test_issue28.html").toString();
     const originalDoc = cheerio.load(html);
-    const naughtyElements = originalDoc('.retweet');
+    const naughtyElements = originalDoc(".retweet");
     expect(naughtyElements.length).toEqual(2);
 
     const newDoc = cleaner(originalDoc);
-    const newNaughtyElements = newDoc('.retweet');
+    const newNaughtyElements = newDoc(".retweet");
     expect(newNaughtyElements.length).toEqual(0);
   });
 
-  it("removes trash line breaks that wouldn't be rendered by the browser", () => {
-    const html = fs.readFileSync('tests/fixtures/test_sec1.html').toString();
+  test("removes trash line breaks that wouldn't be rendered by the browser", () => {
+    const html = fs.readFileSync("tests/fixtures/test_sec1.html").toString();
     const originalDoc = cheerio.load(html);
     const newDoc = cleaner(originalDoc);
 
-    const pElements = newDoc('p');
-    const cleanedParaText = pElements[9].children[0].data;
+    const pElements = newDoc("p");
+    const cleanedParaText = pElements.get(9).children[0].data;
     expect(cleanedParaText?.trim()).toEqual(
-      '“This transaction would not only strengthen our global presence, but also demonstrate our commitment to diversify and expand our U.S. commercial portfolio with meaningful new therapies,” said Russell Cox, executive vice president and chief operating officer of Jazz Pharmaceuticals plc. “We look forward to ongoing discussions with the FDA as we continue our efforts toward submission of an NDA for defibrotide in the U.S. Patients in the U.S. with severe VOD have a critical unmet medical need, and we believe that defibrotide has the potential to become an important treatment option for these patients.”'
+      "“This transaction would not only strengthen our global presence, but also demonstrate our commitment to diversify and expand our U.S. commercial portfolio with meaningful new therapies,” said Russell Cox, executive vice president and chief operating officer of Jazz Pharmaceuticals plc. “We look forward to ongoing discussions with the FDA as we continue our efforts toward submission of an NDA for defibrotide in the U.S. Patients in the U.S. with severe VOD have a critical unmet medical need, and we believe that defibrotide has the potential to become an important treatment option for these patients.”"
     );
   });
 
-  it('inlines code blocks as text', () => {
-    const html = fs.readFileSync('tests/fixtures/test_github1.html').toString();
+  test("inlines code blocks as text", () => {
+    const html = fs.readFileSync("tests/fixtures/test_github1.html").toString();
     const originalDoc = cheerio.load(html);
-    const codeElements = originalDoc('code');
+    const codeElements = originalDoc("code");
     expect(codeElements.length).toEqual(26);
 
     const newDoc = cleaner(originalDoc);
-    const newCodeElements = newDoc('code');
+    const newCodeElements = newDoc("code");
     expect(newCodeElements.length).toEqual(0);
 
     expect(
-      newDoc('body')
+      newDoc("body")
         .text()
         .indexOf("extractor = require('extract-site-metadata');")
     ).toBeGreaterThan(0);
--- tests/formatter.test.ts
+++ tests/formatter.test.ts
@@ -1,31 +1,31 @@
-import cheerio from 'cheerio';
-import fs from 'fs';
-import formatter from '../src/formatter';
+import cheerio from "cheerio";
+import fs from "fs";
+import formatter from "../src/formatter";
 
-describe('Formatter', () => {
-  it('exists', () => {
+describe("Formatter", () => {
+  it("exists", () => {
     expect(formatter).toBeDefined();
   });
 
-  it('replaces links with plain text', () => {
+  it("replaces links with plain text", () => {
     const html = fs
-      .readFileSync('tests/fixtures/test_businessWeek1.html')
+      .readFileSync("tests/fixtures/test_businessWeek1.html")
       .toString();
     const originalDoc = cheerio.load(html);
 
-    expect(originalDoc('a').length).toEqual(223);
+    expect(originalDoc("a").length).toEqual(223);
 
-    formatter(originalDoc, originalDoc('body'), 'en');
-    expect(originalDoc('a').length).toEqual(0);
+    formatter(originalDoc, originalDoc("body"), "en");
+    expect(originalDoc("a").length).toEqual(0);
   });
 
   it("doesn't drop text nodes accidentally", () => {
     const html = fs
-      .readFileSync('tests/fixtures/test_wikipedia1.html')
+      .readFileSync("tests/fixtures/test_wikipedia1.html")
       .toString();
     const doc = cheerio.load(html);
 
-    formatter(doc, doc('body'), 'en');
+    formatter(doc, doc("body"), "en");
     const newHtml = doc.html();
     expect(
       /is a thirteen episode anime series directed by Akitaro Daichi and written by Hideyuki Kurata/.test(
